9c6d1f7a9d711600fdf7fa5a54976dd29d7bb78d
CC-4477 <sameer.m.iyengar@gmail.com>
2020-04-29 01:13:45 -0700

    site colors and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,32 @@
-// tailwind.config.js
-/*
 module.exports = {
-  theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
-    fontFamily: {
-      display: ['Bahnschrift', 'sans-serif'],
-      body: ['Barlow', 'sans-serif'],
-    },
-    borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '4': '4px',
-    },
-    extend: {
-      colors: {
-        baseteal: '#9cdbff',
-        dteal: '',
-        lteal: ''
-      },
-      spacing: {
-        '96': '24rem',
-        '128': '32rem',
-      }
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        fontFamily: {
+            display: ['Bahnschrift', 'sans-serif'],
+            body: ['Barlow', 'sans-serif'],
+        },
+        borderWidth: {
+            default: '1px',
+            '0': '0',
+            '2': '2px',
+            '4': '4px',
+        },
+        extend: {
+            colors: {
+                baseteal: '#9cdbff',
+                dteal: '5EA6B0',
+                lteal: '#5EA6B0',
+                backgroundColor: theme => ({
+                    'primary': '#5EA6B0',
+                    'secondary': '#ffed4a',
+                    'danger': '#e3342f',
+                })
+            }
+        }
     }
-  }
 }
-*/
