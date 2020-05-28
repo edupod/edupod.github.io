452570c6d2e4b7e2930895f760fe10907546ac10
CC-4477 <sameer.m.iyengar@gmail.com>
2020-05-27 20:55:35 -0700

    rails

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
-// postcss.config.js
-
 module.exports = {
-    plugins: [
-        // ...
-        require("tailwindcss"),
-        require("autoprefixer"),
-
-        // ...
-    ],
-};
+  plugins: [
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+}
