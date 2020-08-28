753a70f41b7e9af7a289198ab800249eecda3d3f
CC-4477 <sameer.m.iyengar@gmail.com>
2020-08-28 00:17:53 -0700

    added graphics, init angular

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            loader: 'postcss-loader',
+            options: {
+                ident: 'postcss',
+                syntax: 'postcss-scss',
+                plugins: () => [
+                    require('postcss-import'),
+                    require('tailwindcss'),
+                    require('autoprefixer'),
+                ]
+            }
+        }]
+    }
+};
