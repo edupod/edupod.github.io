023dafe149cd746048dc2bc4585bc2688ec4b697
CC-4477 <sameer.m.iyengar@gmail.com>
2021-04-05 12:47:48 -0800

    completed template for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,5 @@
 module.exports = {
     theme: {
-        screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-        },
-        fontFamily: {
-            display: ['Bahnschrift', 'sans-serif'],
-            body: ['Barlow', 'sans-serif'],
-            h1: ['Barlow', 'sans-serif', 'bold-700']
-        },
-        borderWidth: {
-            default: '1px',
-            '0': '0',
-            '2': '2px',
-            '4': '4px',
-        },
         extend: {
             colors: {
                 baseteal: '#9cdbff',
@@ -24,11 +7,30 @@ module.exports = {
                 lteal: '#5EA6B0',
                 textteal: '#0C3C3F',
                 ddteal: '#003333',
-                backgroundColor: theme => ({
-                    'primary': '#5EA6B0',
-                    'secondary': '#ffed4a',
-                    'danger': '#e3342f',
-                }),
+                vlteal: {
+                    DEFAULT: '#18CCCC',
+                    '50': '#D0F9F9',
+                    '100': '#B9F7F7',
+                    '200': '#8CF1F1',
+                    '300': '#5EECEC',
+                    '400': '#30E7E7',
+                    '500': '#18CCCC',
+                    '600': '#139E9E',
+                    '700': '#0D7171',
+                    '800': '#084343',
+                    '900': '#031515'
+                },
+            },
+            fontFamily: {
+                display: ['Bahnschrift', 'sans-serif'],
+                body: ['Barlow', 'sans-serif'],
+                h1: ['Barlow', 'sans-serif', 'bold-700']
+            },
+            borderWidth: {
+                default: '1px',
+                '0': '0',
+                '2': '2px',
+                '4': '4px',
             }
         }
     }
